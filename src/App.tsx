5fb8976817842b8bc737234164dec80afa9611ec
MilesRoark <146493186+MilesRoark@users.noreply.github.com>
2025-01-02 02:31:20 -0500

    KoboLandingPage

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Features from "./Sections/Features";
+import Footer from "./Sections/Footer";
+import Hero from "./Sections/Hero";
+import Nav from "./Sections/Nav";
+import Partners from "./Sections/Partners";
+import Reviews from "./Sections/Reviews";
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Features />
+      <Partners />
+      <Reviews />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
